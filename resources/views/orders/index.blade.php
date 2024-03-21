1c652fa02c58d3c14b85f7f52be05f383e95bb45
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-03-21 22:58:17 +0200

    Orders blade done

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,119 +1,39 @@
 @extends('app')
 @section('title', 'Orders')
 @section('body')
-<div class="body">
-    <h1 class="mt-10 text-center text-4xl">Orders</h1>
-    <div class="grid grid-cols-1">
-        @foreach ($orders as $order)
-        @php($total = 0)
-        <div class="m-5 shadow-md">
-            <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
-                <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Order ID
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Order Name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Order Status
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Order Address
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="bg-blue-500 border-b border-blue-400">
-                        <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                            {{ $order->id }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $order->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $order->status }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $order->address }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
-                <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Item ID
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Name
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Description
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Category
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Price
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Quantity
-                        </th>
-                        <th scope="col" class="px-6 py-3">
-                            Item Total
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($order->items as $item)
-                    <tr class="bg-blue-500 border-b border-blue-400">
-                        <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                            {{ $item->id }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $item->name }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $item->description }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $item->category }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $item->price }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $item->pivot->quantity }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $item->price * $item->pivot->quantity }}
-                            @php($total += $item->price * $item->pivot->quantity)
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
-                <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
-                    <tr>
-                        <th scope="col" class="px-6 py-3">
-                            Order Total
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="bg-blue-500 border-b border-blue-400">
-                        <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
-                            {{ $total }}
-                        </th>
-                    </tr>
-                </tbody>
-            </table>
+    <div class="body">
+        <h1 class="mt-10 text-center text-4xl">Orders</h1>
+        <div class="grid grid-cols-3 gap-10 m-10">
+            @foreach ($orders as $order)
+                <div class="bg-blue-300 rounded-lg">
+                    @php($total = 0)
+                    <div class="grid grid-cols-1 bg-blue-400 rounded-lg p-5 m-5">
+                        <div class="text-2xl text-center m-1">{{ $order->name }}</div>
+                        <div class="text-lg">Status : {{ $order->status }}</div>
+                        <div class="text-lg">Address : {{ $order->address }}</div>
+                    </div>
+                    <div class="grid grid-cols-5 gap-4 text-center bg-blue-400 rounded-lg p-5 m-5">
+                        <div class="font-bold">Name</div>
+                        <div class="font-bold">Category</div>
+                        <div class="font-bold">Price</div>
+                        <div class="font-bold">Quantity</div>
+                        <div class="font-bold">Total</div>
+                        <hr class="col-span-5">
+                        @foreach ($order->items as $item)
+                            <div>{{ $item->name }}</div>
+                            <div>{{ $item->category }}</div>
+                            <div>{{ $item->price }}</div>
+                            <div>{{ $item->pivot->quantity }}</div>
+                            <div>{{ $item->price * $item->pivot->quantity }}
+                                @php($total += $item->price * $item->pivot->quantity)</div>
+                        @endforeach
+                        <hr class="col-span-5">
+                        <div class="col-span-1 font-bold">Order Total</div>
+                        <div class="col-span-3"></div>
+                        <div class="col-span-1">{{ $total }}</div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
-</div>
 @endsection
